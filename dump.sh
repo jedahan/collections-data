fb4063c41211df9b9a47fcefaee8db2b08f6f8da
Jonathan Dahan <jonathan@jedahan.com>
2013-01-28 20:18:00 -0500

    do do

--- dump.sh
+++ dump.sh
@@ -1,9 +1,9 @@
 #!env zsh
 
-for page in ids/*.json
+for page in ids/*.json; do
   echo "Dumping page ${page%.json}"
-  for id in `jq '.ids[]' < ${page}`
-    rm object/${id}.json
+  for id in `jq '.ids[]' < ${page}`; do
+    rm object/${id}.json &> /dev/null
     http http://165.225.128.86/object/${id} > object/${id}.json &
   done
   wait
